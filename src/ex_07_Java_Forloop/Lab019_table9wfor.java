4eb5fb471b7865612269b7052f4497aac9fd2c0e
Prateek Sharma <sharmaprateek24@gmail.com>
2025-03-19 14:26:12 +0530

    19-March-25-Lab019 added

---
+++ src/ex_07_Java_Forloop/Lab019_table9wfor.java
@@ -0,0 +1,15 @@
+package ex_07_Java_Forloop;
+
+public class Lab019_table9wfor {
+    public static void main(String[] args) {
+        int a=9;
+        System.out.println("Table of "+a+":");
+        for (int i=1;i<=10;i++)
+        {
+            //System.out.println(a"*"i=);
+            System.out.println(a+"x"+i+"="+(a*i));
+        }
+    }
+
+
+}
