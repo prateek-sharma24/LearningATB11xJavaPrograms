97a818f5532dad5330bbe0a84f672606d4fcbee7
Prateek Sharma <sharmaprateek24@gmail.com>
2025-04-05 13:47:23 +0530

    5-4-25>Inheritance-part2

---
+++ src/Lab012_Inheritance/Hybrid_Inheritance/Lab030_hybrid.java
@@ -0,0 +1,12 @@
+package Lab012_Inheritance.Hybrid_Inheritance;
+
+public class Lab030_hybrid {
+    //not possible by using classes
+    }
+class Gr
+{
+
+}
+class Father extends Gr
+{}
+class Mother extends Gr{}
---
+++ src/Lab012_Inheritance/hierachical/Lab029_heirarchical.java
@@ -0,0 +1,51 @@
+package Lab012_Inheritance.hierachical;
+
+
+
+public class Lab029_heirarchical {
+    public static void main(String[] args) {
+        fther f1 = new fther();
+        f1.f();
+        Prateek p1 = new Prateek();
+        p1.p();
+        p1.f();
+        Stuti s1 = new Stuti();
+        s1.s();
+        s1.f();
+        Jyoti j1 = new Jyoti();
+        j1.j();
+        j1.f();
+
+
+    }
+}
+    class  fther
+    {
+        void f()
+        {
+            System.out.println("3BHK");
+        }
+    }
+    class Prateek extends fther
+
+    {
+        void p()
+        {
+            System.out.println("h2-Prateek");
+        }
+    }
+    class Stuti extends fther
+    {
+        void s()
+        {
+            System.out.println("h3-Stuti");
+        }
+    }
+    class Jyoti extends fther
+    {
+        void j()
+        {
+            System.out.println("h4-Jyoti");
+        }
+    }
+
